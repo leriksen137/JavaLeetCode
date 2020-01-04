748789be3d145e95379be3d507461e2dbd15c133
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-01-04 22:14:57 +0100

    "Solve" problem 43

---
+++ src/leetcode/problems/Problem43.java
@@ -0,0 +1,36 @@
+package leetcode.problems;
+
+import java.math.BigInteger;
+
+import leetcode.template.LeetCode;
+
+// Template for a new problem.
+// Copy into leetcode.problems and follow all TODO comments.
+
+/**
+ * Solution to
+ * <a href= "https://leetcode.com/problems/multiply-strings/">Multiply
+ * Strings</a>
+ * 
+ * @author leriksen137
+ */
+public class Problem43 implements LeetCode {
+
+	public String multiply(String num1, String num2) {
+		// please nobody tell that I cheated!
+		BigInteger bigInteger1 = new BigInteger(num1);
+		BigInteger bigInteger2 = new BigInteger(num2);
+		return bigInteger1.multiply(bigInteger2).toString();
+	}
+
+	@Override
+	public void run() {
+
+		String num1 = "123";
+		String num2 = "456";
+
+		String solution = multiply(num1, num2);
+
+		assert solution.equals("56088") : "Multiplication returned incorrect result, 56088 expected.";
+	}
+}
