0084685e6d34cf4f3aea45865e951f7ecee7a885
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-01-06 22:40:24 +0100

    Add incomplete solutions to problems 31 and 32

---
+++ src/leetcode/problems/Problem31.java
@@ -0,0 +1,36 @@
+package leetcode.problems;
+
+import leetcode.template.LeetCode;
+
+// Template for a new problem.
+// Copy into leetcode.problems and follow all TODO comments.
+
+/**
+ * Solution to <a href= "https://leetcode.com/problems/next-permutation/">Next
+ * Permutation</a>
+ * 
+ * @author leriksen137
+ */
+public class Problem31 implements LeetCode {
+
+	public void nextPermutation(int[] nums) {
+		int temp;
+		for (int i = nums.length - 1; i > 0; i--) {
+			temp = nums[i];
+			nums[i] = nums[i - 1];
+			nums[i - 1] = temp;
+			if (nums[i - 1] > nums[i]) {
+				return;
+			}
+		}
+	}
+
+	// NOT SOLVED YET
+	@Override
+	public void run() {
+		int[] nums = new int[] { 2, 3, 4, 1 };
+		// 3 1 2 4
+
+		assert nums[0] == 2 : "Wrong solution";
+	}
+}
--- src/leetcode/problems/Problem32.java
+++ src/leetcode/problems/Problem32.java
@@ -1,5 +1,8 @@
 package leetcode.problems;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 import leetcode.template.LeetCode;
@@ -18,53 +21,65 @@ public class Problem32 implements LeetCode {
 
 	// probably has to be O(n)
 	public int longestValidParentheses(String s) {
-		// tracks the best solution
-		int solution = 0;
+		// tracks the current best solution separated by "levels"
+		List<Integer> solutionByLevel = new ArrayList<>();
 
-		// tracks the current solution with elements on the stack
-		int currentSolution = 0;
-
+		// stack used to push and pop brackets on
 		Stack<Character> bracketStack = new Stack<>();
 
-		char c;
+		char poppedChar = ' ';
+		int collapseSum = 0;
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == '(') {
 				bracketStack.push('(');
 			} else { // it's ')'
-				if (bracketStack.isEmpty()) {
-					if (currentSolution > solution) {
-						solution = currentSolution;
-					}
-					currentSolution = 0;
-				} else {
-					c = bracketStack.pop();
-					if (c == '(') {
-						currentSolution = currentSolution + 2;
+				if (!bracketStack.isEmpty()) {
+					poppedChar = bracketStack.pop();
+					if (poppedChar == '(') {
+						if (solutionByLevel.size() > bracketStack.size()) {
+							collapseSum = solutionByLevel.get(bracketStack.size());
+						} else {
+							collapseSum = 0;
+						}
+
+						while (solutionByLevel.size() > bracketStack.size()) {
+							collapseSum = collapseSum + solutionByLevel.get(solutionByLevel.size() - 1);
+							solutionByLevel.remove(solutionByLevel.size() - 1);
+						}
+						solutionByLevel.add(collapseSum + 2);
 					}
 				}
 			}
-
-		}
-		if (currentSolution > solution) {
-			solution = currentSolution;
 		}
 
-		return solution;
+		if (solutionByLevel.isEmpty()) {
+			return 0;
+		} else {
+			return Collections.max(solutionByLevel);
+		}
 	}
 
 	@Override
 	public void run() {
 
-		String s1 = "(()";
-		int solution = longestValidParentheses(s1);
+		String s0 = "())";
+		int solution = longestValidParentheses(s0);
 		assert solution == 2 : "Result should be 2.";
 
-		String s2 = "()(()";
+		String s1 = "(()()";
+		solution = longestValidParentheses(s1);
+		assert solution == 4 : "Result should be 4 for " + s1;
+
+		String s2 = "()((())";
 		solution = longestValidParentheses(s2);
+		assert solution == 4 : "Result should be 4 for " + s2;
+
+		String s3 = "()(()";
+		solution = longestValidParentheses(s3);
 		assert solution == 2 : "Result should be 2.";
 
-		String s3 = "())((()()(()(((()())))))))((((((())()(()()(())()))(()))(()()())((((((()())()()()(()))())(((()(()(())(()((()())())))((()(((()(()((()())())))(())))()))))))))))())()))((())()()()()))((()))(((()))()(()))()((()()()(()))(((()()()()((()()(()())))(())))))))()))))()))()((()())())(()))(((()()()((())())())(((())((()))(())(())()))))(())))()())())()(()))))(())))(())))(()((())(())))((()(()))((((()))))()((()()()())()(())(()(()()())()((())(()((()()()())()))((()))))()))())))))))()((()()))()()()()))(()()()()(())()(()))))()(((((()(((((((()(((())()))(()())()(()))(()(()((()((((()))))()(())(()))))()(()(((((((()))((()(()(()))(()())(()())()(())()(()((((()))))()()()(((())()())()))())()(())))(()()))))(())))))((((()))))(((((()))((((()))((())))))((())))())((((((((()(()((())))()))()()))))))))(((()(()())(()))((())((())(()))))))))((())()()())(()(())()))))((())())(()(())((()())))))((())((())())()(()))(())()()()((((()(()()))()(()(((((()())())())))())()))()))(()())()((()()()()((())()())(((())(((()(()()(((((((()(((()((((()(()((())))())())(())()(()())()((())()()()))()(()())()))()())))()()())))()(((((()()))(())(((()())))(())(())())(())(()()())))()())()))(()))))(()())()(()))((((())()())(())(((()())(()((((((((()))((((())())()((()()())())()()()())()()((()())(()()))((()()))()))(((()((()()(()))()(())()())((())())()(((())))(())((()((())(()()()(()(()))((()((((()(()()(())(()()))))(())()(()()))))())()((())(()))()()(()()()((()))(()))))())((()(()((";
-		solution = longestValidParentheses(s3);
+		String s4 = "())((()()(()(((()())))))))((((((())()(()()(())()))(()))(()()())((((((()())()()()(()))())(((()(()(())(()((()())())))((()(((()(()((()())())))(())))()))))))))))())()))((())()()()()))((()))(((()))()(()))()((()()()(()))(((()()()()((()()(()())))(())))))))()))))()))()((()())())(()))(((()()()((())())())(((())((()))(())(())()))))(())))()())())()(()))))(())))(())))(()((())(())))((()(()))((((()))))()((()()()())()(())(()(()()())()((())(()((()()()())()))((()))))()))())))))))()((()()))()()()()))(()()()()(())()(()))))()(((((()(((((((()(((())()))(()())()(()))(()(()((()((((()))))()(())(()))))()(()(((((((()))((()(()(()))(()())(()())()(())()(()((((()))))()()()(((())()())()))())()(())))(()()))))(())))))((((()))))(((((()))((((()))((())))))((())))())((((((((()(()((())))()))()()))))))))(((()(()())(()))((())((())(()))))))))((())()()())(()(())()))))((())())(()(())((()())))))((())((())())()(()))(())()()()((((()(()()))()(()(((((()())())())))())()))()))(()())()((()()()()((())()())(((())(((()(()()(((((((()(((()((((()(()((())))())())(())()(()())()((())()()()))()(()())()))()())))()()())))()(((((()()))(())(((()())))(())(())())(())(()()())))()())()))(()))))(()())()(()))((((())()())(())(((()())(()((((((((()))((((())())()((()()())())()()()())()()((()())(()()))((()()))()))(((()((()()(()))()(())()())((())())()(((())))(())((()((())(()()()(()(()))((()((((()(()()(())(()()))))(())()(()()))))())()((())(()))()()(()()()((()))(()))))())((()(()((";
+		solution = longestValidParentheses(s4);
 
 	}
 }
