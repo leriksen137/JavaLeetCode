51733add9e59f333f417e016d767034b429b089c
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-01-09 12:05:45 +0100

    Solved Problem 31

--- src/leetcode/problems/Problem31.java
+++ src/leetcode/problems/Problem31.java
@@ -1,5 +1,7 @@
 package leetcode.problems;
 
+import java.util.Arrays;
+
 import leetcode.template.LeetCode;
 import leetcode.template.LeetCodeAnnotation;
 
@@ -11,7 +13,29 @@ import leetcode.template.LeetCodeAnnotation;
 public class Problem31 implements LeetCode {
 
 	public void nextPermutation(int[] nums) {
+		int startOfAscending = nums.length - 2;
+		while (startOfAscending >= 0 && nums[startOfAscending] >= nums[startOfAscending + 1])
+			startOfAscending--;
+		if (startOfAscending == -1) {
+			Arrays.sort(nums);
+		} else {
+			for (int j = nums.length - 1; j > startOfAscending; j--) {
+				if (nums[j] > nums[startOfAscending]) {
+					swap(nums, startOfAscending, j);
+					break;
+				}
+			}
 
+			for (int k = startOfAscending + 1, l = nums.length - 1; l > k; k++, l--) {
+				swap(nums, k, l);
+			}
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int temp = nums[j];
+		nums[j] = nums[i];
+		nums[i] = temp;
 	}
 
 	@Override
@@ -20,7 +44,6 @@ public class Problem31 implements LeetCode {
 
 		nextPermutation(nums);
 
-		assert nums != nums : "Assertion failed in " + this.getClass().getName();
-
+		assert Arrays.equals(nums, new int[] { 3, 1, 2, 4 }) : "Assertion failed in " + this.getClass().getName();
 	}
 }
