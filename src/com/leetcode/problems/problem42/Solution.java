e665c746ff80d96d0c474e48dbb002b247731a92
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-01-17 13:32:54 +0100

    Add skeleton code for solution 42

--- src/com/leetcode/problems/problem42/Solution.java
+++ src/com/leetcode/problems/problem42/Solution.java
@@ -8,6 +8,23 @@ import com.leetcode.framework.annotations.LeetCodeProblem;
 @LeetCodeProblem(problemNumber = 42, problemName = "Trapping Rain Water")
 public class Solution {
 	public int trap(int[] height) {
-		return 0;
+		int waterVolume = 0;
+
+		int waterLevel = 1;
+		boolean foundWater = true;
+		int[] boundaries = new int[2];
+		while (foundWater) {
+			foundWater = findLeftAndRightBoundariesForWaterlevel(waterLevel, boundaries);
+			waterLevel++;
+		}
+
+		return waterVolume;
+	}
+
+	private boolean findLeftAndRightBoundariesForWaterlevel(int waterLevel, int[] boundaries) {
+		int left = 0;
+		int right = 0;
+
+		return right == 0;
 	}
 }
