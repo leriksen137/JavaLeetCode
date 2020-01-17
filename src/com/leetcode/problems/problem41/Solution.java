df2a1bfdc06bf816330fc94ec943ad5b062a7729
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-01-17 20:01:10 +0100

    remove code skeleton

--- src/com/leetcode/problems/problem41/Solution.java
+++ src/com/leetcode/problems/problem41/Solution.java
@@ -8,15 +8,6 @@ import com.leetcode.framework.annotations.LeetCodeProblem;
 @LeetCodeProblem(problemNumber = 41, problemName = "First Missing Positive")
 public class Solution {
 	public int firstMissingPositive(int[] nums) {
-		int firstMissingPositive = 1;
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] < 1) {
-				// noop, can Ignore
-			} else {
-				// TODO write some code here
-			}
-		}
-
-		return firstMissingPositive;
+		return 0;
 	}
 }
