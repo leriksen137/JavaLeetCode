0f3cb853b9c6b337903593e5967b8d900d7aa46a
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-02-02 22:02:10 +0100

    Solve Problem 138

--- src/com/leetcode/problems/problem138/Solution.java
+++ src/com/leetcode/problems/problem138/Solution.java
@@ -1,5 +1,8 @@
 package com.leetcode.problems.problem138;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.leetcode.framework.annotations.LeetCodeProblem;
 import com.leetcode.framework.datastructures.Node;
 
@@ -9,6 +12,20 @@ import com.leetcode.framework.datastructures.Node;
 @LeetCodeProblem(problemNumber = 138, problemName = "Copy List with Random Pointer")
 public class Solution {
 	public Node copyRandomList(Node head) {
-		return new Node(0);
+		Map<Node, Node> map = new HashMap<>();
+		Node pointer = head;
+		while (pointer != null) {
+			map.put(pointer, new Node(pointer.val));
+			pointer = pointer.next;
+		}
+
+		pointer = head;
+		while (pointer != null) {
+			map.get(pointer).next = map.get(pointer.next);
+			map.get(pointer).random = map.get(pointer.random);
+			pointer = pointer.next;
+		}
+
+		return map.get(head);
 	}
 }
