8d58ac0e965f8183d9be0074ef3118bac4cc8761
Laurent Eriksen <laurent.eriksen@gmail.com>
2020-02-02 21:06:08 +0100

    Solve Problem 78

--- src/com/leetcode/problems/problem78/Solution.java
+++ src/com/leetcode/problems/problem78/Solution.java
@@ -1,6 +1,6 @@
 package com.leetcode.problems.problem78;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.leetcode.framework.annotations.LeetCodeProblem;
@@ -11,6 +11,27 @@ import com.leetcode.framework.annotations.LeetCodeProblem;
 @LeetCodeProblem(problemNumber = 78, problemName = "Subsets")
 public class Solution {
 	public List<List<Integer>> subsets(int[] nums) {
-		return Collections.emptyList();
+		int n = nums.length;
+		int numberOfSubsets = pow(2, n);
+		List<List<Integer>> solution = new ArrayList<>();
+		for (int k = 0; k < numberOfSubsets; k++) {
+			List<Integer> subset = new ArrayList<>();
+			for (int i = 0; i < n; i++) {
+				if (isIthBinaryDigitOfKEqualToOne(i, k)) {
+					subset.add(nums[i]);
+				}
+			}
+			solution.add(subset);
+		}
+
+		return solution;
+	}
+
+	private int pow(int base, int exponent) {
+		return (int) Math.pow(base, exponent);
+	}
+
+	private boolean isIthBinaryDigitOfKEqualToOne(int i, int k) {
+		return (k / pow(2, i)) % 2 == 1;
 	}
 }
